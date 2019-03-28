d0d1f2598ca8562fd84efc6812105cc70b27c795
Jessica Hong <jessicahong.24@gmail.com>
2019-03-28 00:15:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber(number, name){
+  var array = [] 
+  
+  for (var i=0 ; i<number.length; i++){
+  array.push(`Welcome,  ${name}. You are number ${number[i]} in line.`)
+  }
+  
+return array
+}
